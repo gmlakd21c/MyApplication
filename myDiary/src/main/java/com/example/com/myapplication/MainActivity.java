40bbabaf2167ed4f99754027e9277e18b91a7cc9
xvop11 <xvop11@naver.com>
2015-11-28 16:45:07 +0900

    good

--- myDiary/src/main/java/com/example/com/myapplication/MainActivity.java
+++ myDiary/src/main/java/com/example/com/myapplication/MainActivity.java
@@ -53,49 +53,51 @@ public class MainActivity extends AppCompatActivity {
         diaryText = (EditText)findViewById(R.id.diaryText);
         dateText = (TextView)findViewById(R.id.dateText);
         datePicker = (DatePicker)findViewById(R.id.datePicker);
+        saveBtn = (Button)findViewById(R.id.saveBtn);
 
-        Calendar cal = Calendar.getInstance();
+        Calendar cal = Calendar.getInstance(); // 오늘 날짜를가지고온다.
         thisYear = cal.get(Calendar.YEAR);
-        thisMonth = cal.get(Calendar.MONTH);
+        thisMonth = cal.get(Calendar.MONTH)+1;
         thisDay = cal.get(Calendar.DAY_OF_MONTH);
 
-        saveBtn = (Button)findViewById(R.id.saveBtn);
-        File myDir;
+        dateText.setText((thisYear + "년 " + thisMonth + "월 " + thisDay +"일"));
+        fileName = Integer.toString(thisYear) + "_" + Integer.toString(thisMonth+1) + "_" + Integer.toString(thisDay) + ".txt";
+        diaryText.setText(readDiary(fileName)); //해당하는 날짜에 일기가 있으면 가지고 온다.
 
         fileCheck();
 
         final DatePickerDialog.OnDateSetListener dateSetListener = new DatePickerDialog.OnDateSetListener()
         {
             public void onDateSet(DatePicker view, int year, int monthOfYear, int dayOfMonth)
-            {
+            { // datePickerDialog 리스너
                 String msg = String.format("%d_%d_%d", year, monthOfYear + 1, dayOfMonth);
 
                 thisYear = year;
-                thisMonth = monthOfYear;
+                thisMonth = monthOfYear+1;
                 thisDay = dayOfMonth;
-                dateText.setText(msg);
+                dateText.setText(year + "년 " + monthOfYear + "월 " + dayOfMonth +"일"); // 선택한 날짜 textView,setText()
 
                 fileName = Integer.toString(thisYear) + "_" + Integer.toString(thisMonth+1) + "_" + Integer.toString(thisDay) + ".txt";
-                String str = readDiary(fileName);
-                diaryText.setText(str);
+                String str = readDiary(fileName); // 파일 이름을 통해 해당 파일이 있으면 파일의 내용을 str에 저장
+                diaryText.setText(str); // editText.setText()
             }
         };
 
         final DatePickerDialog dateDig = new DatePickerDialog(MainActivity.this, dateSetListener, thisYear, thisMonth, thisDay);
 
-        saveBtn.setOnClickListener(new OnClickListener()
+        saveBtn.setOnClickListener(new OnClickListener() // 저장버튼 클릭리스너
         {
             public void onClick(View v)
             {
-                saveDiary(fileName);
+                saveDiary(fileName); // 저장할 파일이름을 넘겨주고 파일을 저장한다.
             }
         });
 
-        dateText.setOnClickListener(new OnClickListener()
+        dateText.setOnClickListener(new OnClickListener() // textView 클릭리스너
         {
             public void onClick(View v)
             {
-                dateDig.show();
+                dateDig.show(); // 다이얼로그 show()매소드
             }
         });
     }
@@ -106,7 +108,7 @@ public class MainActivity extends AppCompatActivity {
         filePath = strSDpath + "/mydir";
         File myDir;
         String sysDir = Environment.getRootDirectory().getAbsolutePath();
-        File[] sysFiles =  (new File(sysDir)).listFiles();
+        File[] sysFiles =  (new File(sysDir)).listFiles(); // sdcard저장장소의 파일 리스트
 
         String tmpFileName = filePath;
         for(int i=0; i<sysFiles.length; i++)
@@ -116,10 +118,10 @@ public class MainActivity extends AppCompatActivity {
                 if(tmpFileName.equals(sysFiles.toString()))
                     break;
             }
-            else if(i == sysFiles.length-1)
+            else if(i == sysFiles.length-1) // 마지막 까지 없다면
             {
                 myDir = new File(filePath);
-                myDir.mkdir();
+                myDir.mkdir();  // 파일을 생성한다.
             }
         }
     }
@@ -129,11 +131,11 @@ public class MainActivity extends AppCompatActivity {
         try {
             File file;
             String readFile = filePath +"/"+ fileName;
-            file = new File(readFile);
+            file = new File(readFile); // sdcard 저장장소에 fileName에 대한 file객체
             FileOutputStream outPutStream = new FileOutputStream(file);
-            String str = diaryText.getText().toString().trim();
+            String str = diaryText.getText().toString().trim(); // 일기 내용을 가지고 온다.
 
-            outPutStream.write(str.getBytes());
+            outPutStream.write(str.getBytes()); // 일기 내용을 file에 쓴다
             outPutStream.close();
             Toast.makeText(getApplication(), fileName + "이 저장되었습니다.", Toast.LENGTH_SHORT).show();
         }
@@ -141,9 +143,10 @@ public class MainActivity extends AppCompatActivity {
         }
     }
 
+
     String readDiary(String fileName)
     {
-        String diaryStr = null;
+        String diaryStr = null; // 파일에 있는 일기 내용
         FileInputStream inputStream;
         try
         {
@@ -153,11 +156,11 @@ public class MainActivity extends AppCompatActivity {
             inputStream.read(txt);
             diaryStr = (new String(txt)).trim();
             inputStream.close();
-            Toast.makeText(getApplication(), fileName + "을 불러왔습니다.", Toast.LENGTH_SHORT).show();
+            Toast.makeText(getApplication(), fileName + "을 불러왔습니다.", Toast.LENGTH_SHORT).show(); // 일기가 불렸다는 토스트 메세지
         }
         catch(IOException e)
         {
-            diaryText.setHint("일기 내용");
+            diaryText.setHint("일기 내용"); // 일기가 없을시에 일기 내용을 setHint()한다.
         }
         return diaryStr;
     }
@@ -212,19 +215,15 @@ public class MainActivity extends AppCompatActivity {
 
             case R.id.largeSize:
                 fontSize.setSpan(new AbsoluteSizeSpan(120), 0, diaryText.getText().toString().length(), Spannable.SPAN_EXCLUSIVE_EXCLUSIVE);
-
                 break;
 
             case R.id.nomalSize:
                 fontSize.setSpan(new AbsoluteSizeSpan(60), 0, diaryText.getText().toString().length(), Spannable.SPAN_EXCLUSIVE_EXCLUSIVE);
-
                 break;
 
             case R.id.smallSize:
                 fontSize.setSpan(new AbsoluteSizeSpan(30), 0, diaryText.getText().toString().length(), Spannable.SPAN_EXCLUSIVE_EXCLUSIVE);
-
                 break;
-
         }
         return true;
     }
